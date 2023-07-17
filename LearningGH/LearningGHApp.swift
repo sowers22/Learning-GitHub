bc2b3281de5924e7273deed97bf73eb1c62b6285
James Sowerby <jamessowerby22@gmail.com>
2023-07-17 14:04:44 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by James Sowerby on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
